b2be89b6f2ae49eddff6b183532c34d82475cd2b
DavidDelaunay43 <david.delaunay43@gmail.com>
2024-08-25 16:48:00 +0200

    encapsulation attribut path dans JsonFile

--- classes/json_file.py
+++ classes/json_file.py
@@ -1,34 +1,52 @@
 import json
 from pathlib import Path
+from typing import Union
 
 
 class JsonFile:
     
     
-    def __init__(self, path: str | Path) -> None:
-        self.PATH = Path(path)
-        self.NAME = self.PATH.name
+    def __init__(self, path: Union[str, Path]) -> None:
+        self._path = Path(path) 
+        self._name = self._path.name
+
+
+    @property
+    def path(self) -> Path:
+        return self._path
     
     
+    @path.setter
+    def path(self, new_path: Union[str, Path]) -> None:
+        self._path = Path(new_path)
+        self._name = self._path.name
+    
+    
+    @property
+    def name(self) -> str:
+        return self._name
+
+
     def json_to_dict(self) -> dict:
-        with open(self.PATH, 'r', encoding = 'utf-8') as file:
+        with open(self._path, 'r', encoding='utf-8') as file:
             dico = json.load(file)
         return dico
 
 
     def dict_to_json(self, dictionary: dict) -> None:
         dictionary = {key: str(value) if isinstance(value, Path) else value for key, value in dictionary.items()}
-        with open(self.PATH, 'w', encoding = 'utf-8') as file:
-            json.dump(dictionary, file, indent = 4, ensure_ascii = False)
+        with open(self._path, 'w', encoding='utf-8') as file:
+            json.dump(dictionary, file, indent=4, ensure_ascii=False)
         return
     
     
-    def get_value(self, key: str, return_path: bool = False) -> bool | int | float | str | list | dict | Path | None:
-        return self.json_to_dict().get(key) if not return_path else Path(self.json_to_dict().get(key))
+    def get_value(self, key: str, return_path: bool = False) -> Union[bool, int, float, str, list, dict, Path, None]:
+        value = self.json_to_dict().get(key)
+        return Path(value) if return_path and isinstance(value, str) else value
     
     
-    def set_value(self, key: str, value: bool | int | float | str | list | dict | Path | None) -> None:
+    def set_value(self, key: str, value: Union[bool, int, float, str, list, dict, Path, None]) -> None:
         value = str(value) if isinstance(value, Path) else value
-        dictionnary: dict = self.json_to_dict()
+        dictionnary = self.json_to_dict()
         dictionnary[key] = value
         self.dict_to_json(dictionnary)
